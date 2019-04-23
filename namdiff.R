5f4cbc38f8debc8833d63ebaa3e8b1ff4d023205
Harold Petithomme <harold.petithomme@meteo.fr>
2019-04-23 13:31:07 +0000

    Ajout outil namelist
    nam.sh et namdiff.sh extraient les namelists Fortran et comparent 2 versions
    utilisent rewrite.sh et R

---
+++ namdiff.R
@@ -0,0 +1,93 @@
+getnams = function(file)
+{
+	nams = strsplit(grep("namelist/",readLines(file),value=TRUE),"/")
+	names(nams) = sapply(nams,"[",2)
+	for (i in seq(along=nams)) {
+		nams[[i]] = strsplit(nams[[i]][-(1:2)],",")[[1]]
+		ind = which(duplicated(nams[[i]]))
+		if (length(ind) > 0) {
+			message("--> duplicated variables in ",names(nams)[i],"/",file,": ",
+				paste(nams[[i]][ind]," "))
+			nams[[i]] = unique(sort(nams[[i]]))
+		}
+	}
+
+	nams
+}
+
+args = strsplit(commandArgs(trailingOnly=TRUE),split="=")
+cargs = lapply(args,function(x) strsplit(x[-1],split=":")[[1]])
+names(cargs) = sapply(args,function(x) x[1])
+
+namold = getnams(cargs$ficold)
+namnew = getnams(cargs$ficnew)
+
+nomsold = names(namold)
+nomsnew = names(namnew)
+ind = match(nomsnew,nomsold)
+
+if (any(! nomsold %in% nomsnew)) {
+	cat("! --rmblock=",paste(nomsold[! nomsold %in% nomsnew],collapse=","),"\n",
+		sep="")
+}
+
+snam = sprintf("&%s",nomsnew)
+
+if (as.logical(cargs$move)) {
+	for (i in which(! is.na(ind))) {
+		varsnew = namnew[[i]]
+
+		varsmv = character()
+		for (v in varsnew) {
+			iold = sapply(namold,function(x) any(x==v))
+			inew = sapply(namnew,function(x) any(x==v))
+
+			# soit n'est dans aucun bloc, soit est dans le même bloc
+			if (all(! iold) || iold[nomsold == nomsnew[i]]) next
+
+			if (length(which(inew)) > 1) {
+				message("--> variable ",v," in several new blocks (not moved): ",
+					paste(nomsnew[inew],collapse=" "),"\n")
+			} else if (length(which(iold)) > 1) {
+				message("--> variable ",v," in several old blocks (not moved): ",
+					paste(nomsold[iold],collapse=" "),"\n")
+			} else {
+				varsmv = c(varsmv,v)
+				sdep = sprintf("\t%s=@%s",v,nomsold[iold])
+				snam[i] = sprintf("%s,\n%s",snam[i],paste(sdep,collapse=",\n"))
+				namold[[which(iold)]] = namold[[which(iold)]][namold[[which(iold)]]!=v]
+			}
+		}
+
+		if (length(varsmv) > 0) {
+			cat("! vars moved to",nomsnew[i],":",
+				paste(varsnew[varsnew %in% varsmv]),"\n")
+			namnew[[i]] = namnew[[i]][! namnew[[i]] %in% varsmv]
+		}
+	}
+}
+
+for (i in which(! is.na(ind))) {
+	varsnew = namnew[[i]]
+	varsold = namold[[ind[i]]]
+
+	if (any(! varsold %in% varsnew)) {
+		ssup = sprintf("\t%s=--",varsold[! varsold %in% varsnew])
+		snam[i] = sprintf("%s,\n%s",snam[i],paste(ssup,collapse=",\n"))
+	}
+
+	if (any(! varsnew %in% varsold)) {
+		sadd = sprintf("\t%s=++",varsnew[! varsnew %in% varsold])
+		snam[i] = sprintf("%s\n%s",snam[i],paste(sadd,collapse=",\n"))
+	}
+
+	snam[i] = sprintf("%s\n/\n",snam[i])
+	cat(snam[i])
+}
+
+for (i in which(is.na(ind))) {
+	varsnew = namnew[[i]]
+
+	sadd = sprintf("\t%s=++",varsnew)
+	cat(sprintf("&%s\n%s\n/\n",nomsnew[i],paste(sadd,collapse=",\n")))
+}
---
+++ namdiff.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+mitra=~/mitraille
+
+usage()
+{
+	printf "
+Description:
+
+Synopsis:
+	$(basename $0) ficold ficnew [-move] [-h]
+
+Arguments:
+	-move: authorizes moving variables from another namelist
+	-h: displays help and terminates normally
+
+Details:
+	If moving variables is authorized, no moving is done if variable is present \
+in several namelists (duplicated) in the old file. However, if a variable is \
+duplicated in the new file but not in the old one, moving is done (this is \
+dirty).
+
+Exit status:
+	Non 0 in case of error
+	0 if not
+
+Dependencies:
+
+Author:
+	H Petithomme, Meteo France - DR/GMAP/ALGO
+"
+}
+
+ficold=""
+ficnew=""
+move=FALSE
+help=0
+
+if [ $# -eq 0 ] || echo $* | grep -q ' -h'
+then
+	usage
+	exit
+elif [ $# -lt 2 ]
+then
+	echo "Error: input files missing" >&2
+	exit 1
+fi
+
+echo "$*" | grep -qE ' -move' && move=TRUE || move=FALSE
+R --encoding="latin1" --slave -f $mitra/namdiff.R --args ficold=$1 ficnew=$2 \
+	move=$move
